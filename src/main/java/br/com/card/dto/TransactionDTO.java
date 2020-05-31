074304a42d55f290ea04bb9c42e904040030c638
Miguel Lima <miguelmoreiralima@outlook.com>
2020-05-31 15:58:35 -0300

    Usando padrão dto para retornar as transações

--- src/main/java/br/com/card/dto/TransactionDTO.java
+++ src/main/java/br/com/card/dto/TransactionDTO.java
@@ -3,12 +3,12 @@ package br.com.card.dto;
 import br.com.card.entities.enums.CardApplication;
 import br.com.card.entities.enums.PaymentStatus;
 import br.com.card.entity.Transaction;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -21,7 +21,11 @@ public class TransactionDTO  {
     private  static final long serialVersionUID = 1L;
 
     private String id;
+
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
     private LocalTime time;
     private BigDecimal value;
     private CardApplication cardApplication;
