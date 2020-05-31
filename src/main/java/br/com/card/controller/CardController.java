64f9c07f054d0e57c470a76490fabcbff7d86c48
Miguel Lima <miguelmoreiralima@outlook.com>
2020-05-31 15:56:35 -0300

    adicionando endpoint método put

--- src/main/java/br/com/card/controller/CardController.java
+++ src/main/java/br/com/card/controller/CardController.java
@@ -2,15 +2,12 @@ package br.com.card.controller;
 
 import br.com.card.dto.TransactionDTO;
 import br.com.card.entity.Transaction;
-import br.com.card.repository.CardRepository;
 import br.com.card.service.TransactionService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -20,29 +17,33 @@ import java.util.stream.Collectors;
 @RestController
 public class CardController {
 
-    private CardRepository cardRepository;
     private TransactionService service;
 
     @Autowired
-    public CardController(TransactionService service){
+    public CardController(TransactionService service) {
         this.service = service;
 
     }
+
     @RequestMapping(path = "/transactions", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createTransaction(@RequestBody Transaction transaction) {
-      transaction = service.insert(transaction);
-      URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(transaction.getId()).toUri();
-      return ResponseEntity.created(uri).build();
+        transaction = service.insert(transaction);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(transaction.get_id()).toUri();
+        return ResponseEntity.created(uri).build();
     }
+
     @RequestMapping(path = "/transactions/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<TransactionDTO>> findAll() {
         List<Transaction> list = service.findAll();
         List<TransactionDTO> listDto = list.stream().map(x -> new TransactionDTO(x)).collect(Collectors.toList());
-         return ResponseEntity.ok().body(listDto);
+        return ResponseEntity.ok().body(listDto);
     }
-//    @RequestMapping(path = "/transactions/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<Void> update(@RequestBody Transaction transaction, @PathVariable String id) {
-//         transaction = cardRepository.findOne(transaction)
 
+    @RequestMapping(path = "/transactions/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> update(@RequestBody Transaction transactionDTO, @PathVariable("id") ObjectId id) {
+        transactionDTO.set_id(id);
+        service.update(transactionDTO);
+        return ResponseEntity.noContent().build();
+    }
 
 }
