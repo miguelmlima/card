e218c5ee362c09b04659eef59ccfa6d3ae201fee
Miguel Lima <miguelmoreiralima@outlook.com>
2020-05-31 16:00:48 -0300

    criação de exception

---
+++ src/main/java/br/com/card/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.card.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
