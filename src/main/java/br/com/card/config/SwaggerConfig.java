5e208a8542797cb162d5f8cb9cb677e371b1243e
Miguel Lima <miguelmoreiralima@outlook.com>
2020-05-31 17:29:33 -0300

    ajuste no swagger

--- src/main/java/br/com/card/config/SwaggerConfig.java
+++ src/main/java/br/com/card/config/SwaggerConfig.java
@@ -2,34 +2,43 @@ package br.com.card.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.card"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Card-App")
-                .description("Esta API tem como objetivo realizar  e consultar dados do cartão")
-                .version("1.0")
-                .build();
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Cartões API Rest",
+                "API Rest cadastro de Cartões",
+                "1.0",
+                "Terms of Service",
+                new Contact("Miguel Lima", "https://www.linkedin.com/in/miguel-moreira-lima-11813ab7/",
+                        "miguelmoreiralima@outlook.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+        );
+        return apiInfo;
     }
-
 }
--- src/main/java/br/com/card/controller/CardController.java
+++ src/main/java/br/com/card/controller/CardController.java
@@ -3,6 +3,8 @@ package br.com.card.controller;
 import br.com.card.dto.TransactionDTO;
 import br.com.card.entity.Transaction;
 import br.com.card.service.TransactionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +17,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Api(value = "API Rest cartões")
+@CrossOrigin(origins="*")
 public class CardController {
 
     private TransactionService service;
@@ -26,6 +30,7 @@ public class CardController {
     }
 
     @RequestMapping(path = "/transactions", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Cadastra dados do cartão")
     public ResponseEntity<Void> createTransaction(@RequestBody Transaction transaction) {
         transaction = service.insert(transaction);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(transaction.get_id()).toUri();
@@ -33,6 +38,7 @@ public class CardController {
     }
 
     @RequestMapping(path = "/transactions/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Retorna todos os cartões")
     public ResponseEntity<List<TransactionDTO>> findAll() {
         List<Transaction> list = service.findAll();
         List<TransactionDTO> listDto = list.stream().map(x -> new TransactionDTO(x)).collect(Collectors.toList());
@@ -40,6 +46,7 @@ public class CardController {
     }
 
     @RequestMapping(path = "/transactions/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Atualiza dados do cartão")
     public ResponseEntity<Void> update(@RequestBody Transaction transactionDTO, @PathVariable("id") ObjectId id) {
         transactionDTO.set_id(id);
         service.update(transactionDTO);
